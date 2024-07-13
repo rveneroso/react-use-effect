6655ad2bd41075f5f1e154ae952f22b0b3b66568
Renato Veneroso <rveneroso@gmail.com>
2024-07-13 19:43:43 -0300

    Versão final do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [nome, setNome] = useState("");
+  const [senha, setSenha] = useState("");
+  useEffect(() => {
+    document.title = `Título: ${nome}`;
+    console.log("Página renderizada");
+  }, [ nome, senha ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>UseEffect</h1>
+      <input
+        type="text"
+        placeholder="Nome"
+        onChange={(e) => setNome(e.target.value)}
+      >
+      </input>
+      <input 
+        type="password"
+        placeholder="Senha"
+        onChange={(e) => setSenha(e.target.value)}
+      >
+      </input>
+      <p>Nome: <span>{nome}</span></p>
+      <p>Senha: <span>{senha}</span></p>
     </div>
   );
 }
